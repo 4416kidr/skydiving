ad6a427ca94ac0de0c5c2dc19e8695327a3c3adf
NasuHiroto <nasu.hiroto,17@gmail.com>
2023-07-16 12:08:01 +0900

    add HP_management

---
+++ Assets/Scripts/HP_management.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//UI使うときは忘れずに。
+using UnityEngine.UI;
+
+public class HP_management : MonoBehaviour
+{
+    //最大HPと現在のHP。
+    int maxHp = 100;
+    int currentHp;
+    //Sliderを入れる
+    public Slider slider;
+
+    void Start()
+    {
+        //Sliderを満タンにする。
+        slider.value = 100;
+        //現在のHPを最大HPと同じに。
+        currentHp = maxHp;
+        Debug.Log("Start currentHp : " + currentHp);
+    }
+
+    //ColliderオブジェクトのIsTriggerにチェック入れること。
+    private void OnTriggerEnter(Collider collider)
+    {
+        //Objectタグのオブジェクトに触れると発動
+        if (collider.gameObject.tag == "Object")
+        {
+            //ダメージは1～5の中でランダムに決める。
+            int damage = Random.Range(1, 5);
+            Debug.Log("damage : " + damage);
+
+            //現在のHPからダメージを引く
+            currentHp = currentHp - damage;
+            Debug.Log("After currentHp : " + currentHp);
+
+            //最大HPにおける現在のHPをSliderに反映。
+            //int同士の割り算は小数点以下は0になるので、
+            //(float)をつけてfloatの変数として振舞わせる。
+            slider.value = ((float)currentHp / (float)maxHp) * 100; ;
+            Debug.Log("slider.value : " + slider.value);
+        }
+    }
+}
